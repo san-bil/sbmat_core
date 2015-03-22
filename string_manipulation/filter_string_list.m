cb01233d03992bfd0acbc1768f75dfadda0c5a57
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-03-22 19:07:43 +0000

    added unix_pipe function (like filesep), made zfill accept numerics, added filter for empty strings

--- string_manipulation/filter_string_list.m
+++ string_manipulation/filter_string_list.m
@@ -1,5 +1,5 @@
 function out = filter_string_list(list,regex)
 
-filter = @(tmp) ~isempty(regexp(tmp, regex));
+filter = @(tmp) ~isempty(regexp(tmp, regex, 'once'));
 mask = cellfun(filter, list, 'UniformOutput',0);
 out = list(cell2mat(mask));
