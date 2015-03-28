20ed844bcc876200699d7c4662c664b34a4755b5
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-03-28 13:41:01 +0000

    updated string list filters

--- string_manipulation/filter_string_list.m
+++ string_manipulation/filter_string_list.m
@@ -1,5 +1,15 @@
-function out = filter_string_list(list,regex)
+function out = filter_string_list(list,regex,exclusive)
+
+if(~exist('exclusive','var'))
+    exclusive=0;
+end
+
+if(exclusive)
+    mask_mod = @(tmp)~tmp;
+else
+    mask_mod = @(tmp)tmp;
+end
 
 filter = @(tmp) ~isempty(regexp(tmp, regex, 'once'));
 mask = cellfun(filter, list, 'UniformOutput',0);
-out = list(cell2mat(mask));
+out = list(mask_mod(cell2mat(mask)));
