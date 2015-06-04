fa72d9a59ad97a606b09e4a9305f0fc0ea6ff1b5
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-06-04 16:36:56 +0100

    added string manipulation functions

---
+++ string_manipulation/my_regexp.m
@@ -0,0 +1,7 @@
+function [out, matches] = my_regexp(in_str, matcher)
+
+[r_start,r_end]=regexp(in_str,matcher);
+
+out = mat2cell([r_start' r_end'],ones(length(r_start),1),2);
+
+matches = cellfun_uo0(@(tmp)in_str(tmp(1):tmp(2)), out);
