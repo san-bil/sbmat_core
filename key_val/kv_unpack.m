5cbbef6d9711449bb7148bee199b297c08e56011
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-06-29 12:13:32 +0100

    added ability to define exactly what values to unpack from a dict in key_val/kv_unpack.m

--- key_val/kv_unpack.m
+++ key_val/kv_unpack.m
@@ -1,9 +1,15 @@
-function kv_unpack(dict)
+function kv_unpack(dict,varargin)
 
 for i = 1:size(dict,1)
 
     key = dict{i,1};
     val = dict{i,2};
-    assignin('caller', key, val);
     
+    if(~isempty(varargin))
+        if(ismember(key,varargin))
+            assignin('caller', key, val);
+        end
+    else 
+        assignin('caller', key, val);
+    end
 end
