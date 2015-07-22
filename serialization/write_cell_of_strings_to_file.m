6bd9902ca96096af1487412a553b734d2b02aaed
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-07-22 16:42:04 +0100

    added retval to serialization/write_cell_of_strings_to_file.m so it can be used with cellfun; added ssh_tools/default_ssh_config.m; added warning to ssh_tools/default_ssh_key.m if HOME/.ssh/id_rsa doesn't exist

--- serialization/write_cell_of_strings_to_file.m
+++ serialization/write_cell_of_strings_to_file.m
@@ -1,4 +1,4 @@
-function write_cell_of_strings_to_file(filepath,string_arr)
+function out=write_cell_of_strings_to_file(filepath,string_arr)
 touch(filepath);
 fid=fopen(filepath,'a');
 
@@ -11,3 +11,4 @@ xchar = strcat(xchar{:});
 xchar = strcat(xchar(1:end-1),'\n');
 fprintf(fid,strrep(xchar,',',''));
 fclose(fid);
+out=1;
--- ssh_tools/default_ssh_key.m
+++ ssh_tools/default_ssh_key.m
@@ -1,7 +1,10 @@
-function out = default_ssh_key()
+function default_ssh_key_path = default_ssh_key()
 
-out = path_join(home_folder,'.ssh','id_rsa');
+default_ssh_key_path = path_join(home_folder,'.ssh','id_rsa');
 
-if(~exist(out,'file'))
-    warning([mfilename ': Using ' out ' as the default ssh key, however it does not exist!']);
+if(~exist(default_ssh_key_path,'file'))
+    warning_msg = [mfilename ': Using ' default_ssh_key_path ' as the default ssh key, however it does not exist!\n'];
+    warning_msg = [warning_msg '\t\t\tYou''ll need to create one using ssh-keygen, and then push it to all compute nodes using ssh-copy-id.'];
+    warning_msg = [warning_msg '\t\t\tSee e.g. http://www.thegeekstuff.com/2008/11/3-steps-to-perform-ssh-login-without-password-using-ssh-keygen-ssh-copy-id for details.'];
+    warning(sprintf(warning_msg));
 end
