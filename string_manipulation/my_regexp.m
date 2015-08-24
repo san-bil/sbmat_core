19a720da82645a5bee824079629657ddcad0b098
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-08-24 13:32:43 +0100

    added edge case to string_manipulation/my_regexp.m (no matches)

--- string_manipulation/my_regexp.m
+++ string_manipulation/my_regexp.m
@@ -2,6 +2,10 @@ function [out, matches] = my_regexp(in_str, matcher)
 
 [r_start,r_end]=regexp(in_str,matcher);
 
-out = mat2cell([r_start' r_end'],ones(length(r_start),1),2);
+if(~isempty(r_start) && ~isempty(r_end))
+    out = mat2cell([r_start' r_end'],ones(length(r_start),1),2);
 
-matches = cellfun_uo0(@(tmp)in_str(tmp(1):tmp(2)), out);
+    matches = cellfun_uo0(@(tmp)in_str(tmp(1):tmp(2)), out);
+else
+    matches = {};
+end
