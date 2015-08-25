ea9e161dcbfc7656956f54ec249ba401db998e3f
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-08-25 13:27:44 +0100

    forced cell_arrays/cellfun_uo0.m to return a skinny matrix

--- cell_arrays/cellfun_uo0.m
+++ cell_arrays/cellfun_uo0.m
@@ -1,4 +1,4 @@
 function out = cellfun_uo0(f_handle,varargin)
 
 
-out = cellfun(f_handle,varargin{:},'UniformOutput',0);
+out = force_skinny_matrix(cellfun(f_handle,varargin{:},'UniformOutput',0));
