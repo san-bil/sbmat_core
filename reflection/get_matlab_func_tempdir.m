43f3dc4ac2c5372a4f5994acdbeda8c49299b958
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-09-10 11:21:28 +0100

    fixed get_matlab_func_tempdir.m to actually return a random tmp folder, not just the global tmp folder for that function

--- reflection/get_matlab_func_tempdir.m
+++ reflection/get_matlab_func_tempdir.m
@@ -1,4 +1,5 @@
-function out = get_matlab_func_tempdir()
+function [out, tmpdir] = get_matlab_func_tempdir()
 
 mycaller=callerfunc();
 out = path_join('/tmp',['matlab_' mycaller]);
+tmpdir = create_increment_folder(get_simple_date, out); 
