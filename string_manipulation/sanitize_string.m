ca981b1e052984a753b3eb94022bafb3d4978bdb
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-09-30 15:07:03 -0400

    fixed sanitize_string.m to replace spaces with only 1 instance of the sanitizer

--- string_manipulation/sanitize_string.m
+++ string_manipulation/sanitize_string.m
@@ -3,8 +3,12 @@ function [ y ] = sanitize_string( x, sanitizer )
 if(~exist('sanitizer','var'))
     sanitizer='_';
 end
+y=lower(x);
 
-y=strrep(lower(x),' ',sanitizer);
+y=strrep(y,'    ',sanitizer);
+y=strrep(y,'   ',sanitizer);
+y=strrep(y,'  ',sanitizer);
+y=strrep(y,' ',sanitizer);
 
 end
 
