fad762d46d79e46a1c872dd582b1f9d04554bff3
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2015-11-04 23:18:31 -0500

    added unsafe mode for recursive get

--- key_val/kv_get_recursive.m
+++ key_val/kv_get_recursive.m
@@ -1,9 +1,17 @@
-function [ res ] = kv_get_recursive(keylist, kv_map )
+function [ res ] = kv_get_recursive(keylist, kv_map, unsafe, varargin )
+
+if(~exist('unsafe','var'))
+    unsafe=0;
+end
 
 tmp=kv_map;
 for i=1:length(keylist)
 
-    tmp = kv_get(keylist{i},tmp);
+    
+    tmp = kv_get(keylist{i},tmp,varargin{:});
+    if((~iscell(tmp) && size(tmp,1)<2) && unsafe)
+        break;
+    end
     
 end
 res=tmp;
