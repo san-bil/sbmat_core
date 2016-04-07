c89d88e977ac0cf2665898caf940f200f073a305
Sanjay Bilakhia <sanj.bilakhia@gmail.com>
2016-04-07 18:01:09 +0100

    fixed major bug in kv_update

--- key_val/kv_update.m
+++ key_val/kv_update.m
@@ -1,13 +1,13 @@
-function [ updated_parent_dict ] = kv_update(updater_dict, parent_dict)
+function [ parent_dict ] = kv_update(updater_dict, parent_dict)
 
-    updated_parent_dict=parent_dict;
+
     for i = 1:size(updater_dict,1)
         
         if(kv_haskey(updater_dict{i,1},parent_dict))
             fprintf('Overwriting dict value for %s\n',str(updater_dict{i,1}));
         end
         
-        updated_parent_dict = kv_set(updater_dict{i,1},updater_dict{i,2},parent_dict);
+        parent_dict = kv_set(updater_dict{i,1},updater_dict{i,2},parent_dict);
     end
 
 end
